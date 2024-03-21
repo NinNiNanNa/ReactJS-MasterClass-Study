8885866d2fd815d96d25a1498d1104f487f261ca
KIM SO JIN <138649793+NinNiNanNa@users.noreply.github.com>
2024-03-22 03:59:38 +0900

    #8 ANIMATIONS

---
+++ react-masterclass12/README.md
@@ -0,0 +1,18 @@
+# #8 ANIMATIONS
+
+## 8.11 AnimatePresence
+
+[AnimatePresence](https://www.framer.com/docs/animate-presence/)
+
+## 8.12 Slider part One
+
+## 8.13 Slider part Two
+
+슬라이드 next, prev 버튼 번갈아가며 빨리 누를시 애니메이션 동작 문제발생  
+-> npm install framer-motion@^7.2.0 으로 다운그레이드
+
+## 8.14 You Need to Watch This
+
+## 8.15 Final Project part One
+
+## 8.16 Final Project part Two
---
+++ react-masterclass12/src/App.tsx
@@ -0,0 +1,19 @@
+import Card from "./components/Card";
+import Slide from "./components/Slide";
+import Layout1 from "./components/Layout1";
+import Layout2 from "./components/Layout2";
+import Layout3 from "./components/Layout3";
+
+function App() {
+  return (
+    <>
+      {/* <Card /> */}
+      {/* <Slide /> */}
+      {/* <Layout1 /> */}
+      {/* <Layout2 /> */}
+      <Layout3 />
+    </>
+  );
+}
+
+export default App;
